1df52b3fe5f9796d6ba44a04460ee939ff6128a8
Tiago Pimentel <117958583+TiagoFP@users.noreply.github.com>
2023-04-13 16:31:15 -0300

    First Commit
    Introdução do código de cadastro de usuários

---
+++ primeiroprojetoruby.rb
@@ -0,0 +1,23 @@
+#Cadastro de usuários
+# usuário digite seu nome
+print "Digite seu nome: "
+
+
+
+#programa permita que o usuário preencha o que foi pedido
+nome = gets.chomp 
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+print "Digite sua matrícula: "
+matrícula = gets.chomp
+
+#saída final
+#para chamar um dado #{variável}
+puts "Usuário: #{nome} #{sobrenome} 
+Idade: #{idade} anos
+Matrícula: #{matrícula}"
